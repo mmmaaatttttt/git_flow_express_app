b2b060d29576cbc45b4420c998f79b03d47020b7
Matt Lane <lane.matthew@gmail.com>
2021-07-09 08:40:36 -0700

    add routes for / and /random

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require("express");
 const PORT = 3001;
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.json({ ping: "pong" });
+});
+
+app.get("/random", (req, res) => {
+  const random = Math.floor(Math.random() * 100);
+  return res.json({ random });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
 });
