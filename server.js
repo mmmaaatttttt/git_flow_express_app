7a793fe15adde2a49877b9fc7734bd6ea9c36f16
Matt Lane <lane.matthew@gmail.com>
2021-07-09 08:59:25 -0700

    set up auth router and add error handler

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const authRouter = require("./routes/auth");
 
 const PORT = 3001;
 const app = express();
+
+app.use("/auth", authRouter);
+
+app.get("/", (req, res) => {
+  return res.json({ status: "ok" });
+});
+
+app.use((err, req, res, next) => {
+  err.status = err.status || 500;
+  err.message = err.message || "broken!";
+  return res.status(err.status).json({ message: err.message });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
